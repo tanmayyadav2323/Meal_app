98b28815016578db05340df2f7c9f7e9f64d4e30
Tanmay Yadav <tanmayyadav2323@gmail.com>
2022-01-11 21:43:08 +0530

    final commit

--- lib/widgets/categories_item.dart
+++ lib/widgets/categories_item.dart
@@ -23,7 +23,7 @@ class CategoryItem extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: Text(
           title,
-          style: Theme.of(context).textTheme.title,
+          style: Theme.of(context).textTheme.headline6,
         ),
         decoration: BoxDecoration(
             gradient: LinearGradient(colors: [
